152beb502aba675c56e6e8524be27162e13b0a65
Кирилл Панов <lliril_05@list.ru>
2025-02-05 15:26:31 +0300

    Created a file for docker and entered secret data into .env file

--- backend/assistant/settings.py
+++ backend/assistant/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yc)zor2h@7o8%ayis&lku-h2!a1kzgc!z5(r^2#x1kx6t85$mn"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
