5170f3c47e5f1bc9c3ba34bf10e305dac1038cb6
Кирилл Панов <lliril_05@list.ru>
2025-02-21 16:02:13 +0300

    Created the component structure and reset the basic styling of ol, ul, p, section, li

---
+++ mini-app/src/components/styled/Ol/styles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Ol = styled.ol`
+    counter-reset: li;
+    list-style: none;
+    font: 15px 'trebuchet MS', 'lucida sans';
+    padding: 0;
+    margin-bottom: 10px;
+`
---
+++ mini-app/src/components/styled/Ul/styles.tsx
@@ -0,0 +1,38 @@
+import styled, { css } from "styled-components";
+
+interface UlProps {
+    $indent?: string;
+    $align?: string;
+    $fontSize?: string;
+    $lineHeight?: string;
+    $isGridList?: boolean;
+}
+
+const gridList = css<UlProps>`
+  margin-left: ${(props) =>
+    props.$indent ? `-${props.$indent}px` : `-${props.theme.indent}`};
+  margin-top: ${(props) =>
+    props.$indent ? `-${props.$indent}px` : `-${props.theme.indent}`};
+  font-size: 0;
+  line-height: 0;
+  text-align: ${(props) => props.$align || "center"};
+
+  li {
+    display: inline-block;
+    margin-left: ${(props) =>
+      props.$indent ? `${props.$indent}px` : props.theme.indent};
+    margin-top: ${(props) =>
+      props.$indent ? `${props.$indent}px` : props.theme.indent};
+    font-size: ${(props) =>
+      props.$fontSize ? `${props.$fontSize}px` : props.theme.fontSizeDefault};
+    line-height: ${(props) =>
+      props.$lineHeight ? `${props.$lineHeight}px` : "27px"};
+    vertical-align: top;
+  }
+`;
+
+export const Ul = styled.ul<UlProps>`
+  margin: 0;
+  padding: 0;
+  ${(props) => (props.$isGridList ? gridList : "")}
+`;
