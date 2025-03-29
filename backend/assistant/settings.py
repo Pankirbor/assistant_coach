3beb9112082900c2b25aca234fef6b096312d548
Кирилл Панов <lliril_05@list.ru>
2025-03-29 12:26:39 +0300

    Adds collection and storage of statistics

--- backend/assistant/settings.py
+++ backend/assistant/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
     "web",
+    "chastely-revived-grayling.cloudpub.ru",
 ]
 
 
@@ -134,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
